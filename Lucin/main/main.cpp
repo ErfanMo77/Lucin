4523b77717de3cbeb3f317e41e872cae68fdb0a5
ErfanMo77 <43669965+ErfanMo98@users.noreply.github.com>
2021-03-11 20:34:27 +0330

    Graphics hello world!

--- Lucin/main/main.cpp
+++ Lucin/main/main.cpp
@@ -2,7 +2,28 @@
 
 int main() {
 
-	std::cout << "hello";
+	const int imgWidth = 256;
+	const int imgHeight = 256;
+
+	std::cout << "p3\n" << imgWidth << ' ' << imgHeight << "\n255\n";
+
+	for (int j = imgHeight-1; j>=0; --j)
+	{
+		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+		for (int i=0; i<imgWidth; i++)
+		{
+			auto r = double(i) / (imgWidth-1);
+			auto g = double(j) / (imgHeight-1);
+			auto b = 0.3;
+
+			int ir = static_cast<int>(r * 255.999);
+			int ig = static_cast<int>(g * 255.999);
+			int ib = static_cast<int>(b * 255.999);
+
+			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+		}
+	}
+	std::cerr << "\nDone.\n";
 
 	return 0;
 }
