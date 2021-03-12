ee10ae23d5c8e3d1afc45754ef0530102f0bfe3a
ErfanMo77 <43669965+ErfanMo98@users.noreply.github.com>
2021-03-12 18:58:49 +0330

    Added ray and vector3 classes.

--- Lucin/main/main.cpp
+++ Lucin/main/main.cpp
@@ -1,26 +1,20 @@
 #include <iostream>
+#include "vec3.h"
+#include "color.h"
 
 int main() {
 
 	const int imgWidth = 256;
 	const int imgHeight = 256;
 
+
 	std::cout << "p3\n" << imgWidth << ' ' << imgHeight << "\n255\n";
 
-	for (int j = imgHeight-1; j>=0; --j)
-	{
+	for (int j = imgHeight - 1; j >= 0; --j) {
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-		for (int i=0; i<imgWidth; i++)
-		{
-			auto r = double(i) / (imgWidth-1);
-			auto g = double(j) / (imgHeight-1);
-			auto b = 0.3;
-
-			int ir = static_cast<int>(r * 255.999);
-			int ig = static_cast<int>(g * 255.999);
-			int ib = static_cast<int>(b * 255.999);
-
-			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+		for (int i = 0; i < imgWidth; ++i) {
+			color pixel_color(double(i) / (imgWidth - 1), double(j) / (imgHeight - 1), 0.25);
+			write_color(std::cout, pixel_color);
 		}
 	}
 	std::cerr << "\nDone.\n";
