4405eb28c0f1fbda4c3553bf9f8cafc4bc801d94
ErfanMo77 <43669965+ErfanMo98@users.noreply.github.com>
2021-03-25 19:59:14 +0430

    Added depth of field to camera.

--- Lucin/main/main.cpp
+++ Lucin/main/main.cpp
@@ -60,18 +60,26 @@ int main() {
 
 
 	// World
-	auto R = cos(pi / 4);
 	hittable_list world;
 
-	auto material_left = make_shared<lambertian>(color(0, 0, 1));
-	auto material_right = make_shared<lambertian>(color(1, 0, 0));
+	auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+	auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+	auto material_left = make_shared<dielectric>(1.5);
+	auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
-	world.add(make_shared<sphere>(point3(-R, 0, -1), R, material_left));
-	world.add(make_shared<sphere>(point3(R, 0, -1), R, material_right));
+	world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
+	world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
+	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.45, material_left));
+	world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
-	// Camera
+	point3 lookfrom(3, 3, 2);
+	point3 lookat(0, 0, -1);
+	vec3 vup(0, 1, 0);
+	auto dist_to_focus = (lookfrom - lookat).length();
+	auto aperture = 2.0;
 
-	camera cam(90.0, aspect_ratio);
+	camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
 	int index = 0;
 	for (int j = image_height - 1; j >= 0; --j) {
